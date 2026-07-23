c7613610cc3a056845f97ca76fe7fdb77f73fb0d
Ali Arezoomandi <ali.arezoomandi1723@gmail.com>
2026-07-23 19:14:34 +0330

    make add, delete and edit product in admin dashboard

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -6,10 +6,16 @@ app_name = 'admin'
 
 urlpatterns = [
     path("home/", views.AdminDashboardHomeView.as_view(), name="home"),
+    
     path("security-edit/", views.AdminSecurityEditView.as_view(), name="security-edit"),
+    
     path("orders/", views.AdminOrdersView.as_view(), name="orders"),
     path("orders/details", views.AdminOrdersDetailView.as_view(), name="orders-detail"),
+    
     path('products/list', views.AdminProductsListView.as_view(), name='products-list'), 
-    path("products/edit/", views.AdminEditProductsView.as_view(), name="edit-products"),
+    path("products/<int:pk>/detail/edit/", views.AdminProductDetailEditView.as_view(), name="edit-products"),
+    path("products/add-product/", views.AdminAddProductView.as_view(), name="add-product"),
+    path('products/<int:pk>/delete', views.AdminProductsDeleteView.as_view(), name='product-delete'), 
+    
     path("customers/", views.AdminCustomersView.as_view(), name="customers"),
 ]
