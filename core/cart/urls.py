d9c8eeee477e6fd4d3674be522a53b7f0226cd4f
Ali Arezoomandi <ali.arezoomandi1723@gmail.com>
2026-07-24 17:06:48 +0330

    make cart app

---
+++ core/cart/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('session/add-product', views.SessionAddProduct.as_view(), name='session-add-product'),
+]
