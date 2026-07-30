2098a3bd6d5476d22a0f68a6653fd43788abad09
Komeil Ahankoobi <komeilahankoobi@gmail.com>
2026-07-30 09:12:26 +0330

    fixed bug in login an signup

--- core/accounts/validators.py
+++ core/accounts/validators.py
@@ -3,5 +3,5 @@ import re
 
 def validate_iranian_cellphone_number(value):
     pattern = r'^09\d{9}$'
-    if not re.match(pattern, value):
-        raise ValidationError('Enter a valid Iranian cellphone number.')
+    if not re.fullmatch(pattern, value):
+        raise ValidationError('شماره موبایل معتبر نیست. مثال: 09123456789')
