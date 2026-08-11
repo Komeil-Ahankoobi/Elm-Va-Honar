d5b4c902c582cb8f404b069257d23fb6729006b7
Ali Arezoomandi <ali.arezoomandi1723@gmail.com>
2026-08-11 11:28:04 +0330

    converting images to webp format

--- core/shop/models.py
+++ core/shop/models.py
@@ -15,6 +15,7 @@ class ProductStatusType(models.IntegerChoices):
 class ProductCategoryModel(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(allow_unicode=True, unique=True)
+    image = models.ImageField(default="default/آبرنگ.webp", upload_to="categories/img/")
 
     meta_title = models.CharField(max_length=70, blank=True,
         help_text="اگه خالی بمونه از title استفاده می‌شه. حداکثر ۶۰-۷۰ کاراکتر.")
@@ -45,6 +46,8 @@ class ProductCategoryModel(models.Model):
 class ProductBrandModel(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(allow_unicode=True, unique=True)
+    image = models.ImageField(default="default/faber-castell.webp", upload_to="brands/img/")
+
 
     meta_title = models.CharField(max_length=70, blank=True,
         help_text="اگه خالی بمونه از title استفاده می‌شه. حداکثر ۶۰-۷۰ کاراکتر.")
