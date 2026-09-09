93a3550772d3f031aeebfd62652f213188fbf69f
Ali Arezoomandi <ali.arezoomandi1723@gmail.com>
2026-09-09 13:50:42 +0330

    add melipayamak for register/login code

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('register/verify/', views.VerifyRegisterView.as_view(), name='verify_register'),
-
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('login/verify/', views.VerifyLoginView.as_view(), name='verify_login'),
-
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path(
+        "register/verify/", views.VerifyRegisterView.as_view(), name="verify_register"
+    ),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("login/verify/", views.VerifyLoginView.as_view(), name="verify_login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("resend-otp/", views.ResendOTPView.as_view(), name="resend_otp"),
 ]
